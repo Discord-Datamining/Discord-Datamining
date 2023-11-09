21b9313a4785c5c63e33660eeb506588933c1a09
Tiemen <ThaTiemsz@users.noreply.github.com>
2023-11-10 00:04:44 +0100

    9 November 2023 - Build 244905 (a93d43a13b2e08f09b6faad5c8763f803ba91ad7)
    Scripts:
    - c13ef44fec53bc7ec2b3.js
    - ad3185c1026d6b482fda.js
    - c29ef82b33b37e7e515d.js

--- current.js
+++ current.js
@@ -928,6 +928,10 @@
         UNIQUE_USERNAME_TOGGLE_TITLE: "Legacy Username Badge",
         UNIQUE_USERNAME_TOGGLE_SUBTITLE: "Display {username} on your profile.",
         ORIGINALLY_KNOWN_AS: "Originally known as {legacyUsername}",
+        POMELO_ACTION_LABEL_OCF: "Update your username",
+        POMELO_EXISTING_FLOW_PROMPT_OCF: "We are asking everyone to update to unique usernames from discriminators (username**#0000**).",
+        POMELO_SUGGESTIONS_OCF: "Recommended for you",
+        POMELO_CHANGE_LATER_OCF: "You can always change this later.",
         UU_REGISTER_DISPLAY_NAME_HEADER: "What's your name?",
         UU_REGISTER_DISPLAY_NAME_HINT: "You can use emoji and special characters.",
         UU_REGISTER_HEADER: "Next, create an account",
@@ -18126,7 +18130,7 @@
         u = E("782340");
       (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244902", ", Version Hash: ").concat("746ec1b67f28f320286bd0ba20c26374481c726e")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("244905", ", Version Hash: ").concat("a93d43a13b2e08f09b6faad5c8763f803ba91ad7")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20516,8 +20520,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "244902", "244902"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244902")), _ = 0), _
+        let _ = parseInt((e = "244905", "244905"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("244905")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -36418,4 +36422,4 @@
     }
   }
 ]);
-//# sourceMappingURL=3f70cf2f229350552d1f.js.map
+//# sourceMappingURL=ad3185c1026d6b482fda.js.map
