634d8c405f76989702065603c58abb4fa196ad1e
Tiemen <ThaTiemsz@users.noreply.github.com>
2023-11-13 20:44:53 +0100

    13 November 2023 - Build 245164 (4bc33cd779dccb938139b1cb8c252f0ae26d10cb)
    Scripts:
    - c13ef44fec53bc7ec2b3.js
    - 242b8303727caa658e8b.js
    - bec7a5c023661cb1a1a1.js

--- current.js
+++ current.js
@@ -18149,7 +18149,7 @@
         u = E("782340");
       (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("245163", ", Version Hash: ").concat("b646f4c1ea9ff11e44883e08bf8351e31acbbfd9")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("245164", ", Version Hash: ").concat("4bc33cd779dccb938139b1cb8c252f0ae26d10cb")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20509,8 +20509,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "245163", "245163"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("245163")), _ = 0), _
+        let _ = parseInt((e = "245164", "245164"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("245164")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -22033,6 +22033,12 @@
         treatments: [{
           id: 1,
           label: "Enabled",
+          config: {
+            isEnabled: !0
+          }
+        }, {
+          id: 2,
+          label: "Enabled with more captions from post-processing",
           config: {
             isEnabled: !0
           }
@@ -36257,4 +36263,4 @@
     }
   }
 ]);
-//# sourceMappingURL=36cc2da9c42c0da68793.js.map
+//# sourceMappingURL=242b8303727caa658e8b.js.map
