0bd13f444bfe984a7bca433b528b6ef84cb19044
Tiemen <ThaTiemsz@users.noreply.github.com>
2023-11-17 20:51:58 +0100

    17 November 2023 - Build 246869 (9f666c63b53ba0a1075b2de8934009f74fb2820e)
    Scripts:
    - c13ef44fec53bc7ec2b3.js
    - 065970f27b3189a2b8b8.js
    - a4500d05a5d9663fca52.js

--- current.js
+++ current.js
@@ -2606,6 +2606,8 @@
         _LOGIN_STAGE: "Listen In",
         REGISTER: "Register",
         ENTER_PHONE_OR_EMAIL: "Enter phone or email",
+        ENTER_YOUR_PHONE: "Enter your phone number",
+        ENTER_YOUR_EMAIL: "Enter your email",
         ENTER_EMAIL: "Enter email",
         REGISTER_USERNAME_HINT: "You can always change this later!",
         REGISTER_LOGIN_PRIVACY_NOTICE: "[View our Privacy Policy]({privacyURL})",
@@ -18181,7 +18183,7 @@
         u = E("782340");
       (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("246868", ", Version Hash: ").concat("962b969df0febe4d1c14a3cccc6d7578b16043b8")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("246869", ", Version Hash: ").concat("9f666c63b53ba0a1075b2de8934009f74fb2820e")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20462,8 +20464,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "246868", "246868"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("246868")), _ = 0), _
+        let _ = parseInt((e = "246869", "246869"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("246869")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -36294,4 +36296,4 @@
     }
   }
 ]);
-//# sourceMappingURL=9867708326c58a3f67aa.js.map
+//# sourceMappingURL=065970f27b3189a2b8b8.js.map
