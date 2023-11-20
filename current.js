25dbb85ef960808357973f5700c75fb736adf861
Tiemen <ThaTiemsz@users.noreply.github.com>
2023-11-21 00:02:41 +0100

    20 November 2023 - Build 247550 (6d2895f594f918e09a190636eca42e029c39966f)
    Scripts:
    - c13ef44fec53bc7ec2b3.js
    - 25698a2e4deada13687a.js
    - cd567c045fb63d24f9cf.js

--- current.js
+++ current.js
@@ -18218,7 +18218,7 @@
         u = E("782340");
       (0, i.setUpdateRules)(s.default), (0, r.default)(u.default, n.default, T.default), a.default.Emitter.injectBatchEmitChanges(o.unstable_batchedUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("247539", ", Version Hash: ").concat("2115b8db0ba7bf75f2daec81bc74ced88eb98a46")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("247550", ", Version Hash: ").concat("6d2895f594f918e09a190636eca42e029c39966f")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20494,8 +20494,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "247539", "247539"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("247539")), _ = 0), _
+        let _ = parseInt((e = "247550", "247550"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("247550")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -36322,4 +36322,4 @@
     }
   }
 ]);
-//# sourceMappingURL=147db4d2b29bca9ee687.js.map
+//# sourceMappingURL=25698a2e4deada13687a.js.map
