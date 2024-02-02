1344b8e46c1395bf396749d6bf3666ac13e1754a
Tiemen <ThaTiemsz@users.noreply.github.com>
2024-02-02 19:53:16 +0100

    2 February 2024 - Build 263551 (9ce356d4b2363764613195d2f3965fe9ec9c5b04)
    Scripts:
    - shared.e80182260ae6d79c51df.js
    - app.49950d2866599da684bb.js
    - 22918.9f2b9d54bbfc371a4d92.js
    - 21396.259a270b7e3f8803a333.js
    - 13798.6a2a5ac1a86675c94b6c.js
    - 63550.a619020e4c7b3d5be7ac.js
    - 482.6e1c86f88a37a71d42ec.js
    - 19263.fe32553ff71153cb7656.js
    - 10991.d742d0d238c0d99e96ae.js
    - 20117.7c4ea5cd4685b0442b9f.js
    - 58409.1811376ebb7f14b0be53.js
    - 31897.ec700144df6b20f401cb.js
    - 3462.b08583489e001a69805d.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 64999.3c0486790babc24c66a4.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 53706.c13e855ace8621f4cbe9.js
    - 25653.f1981721227784f0166e.js
    - 65632.943fa92709b9910cd5e8.js
    - 46318.26a20b3d6c9d947ee7c5.js
    - 14875.ba44eb6f1396bf220735.js
    - 75851.82c9a7f8176d778029e3.js
    - 5486.1bbb0b409999b3345c7d.js
    - 47387.3a8b4da46182111dbaa7.js
    - 31717.0a0e31f5be65b10ca0d9.js
    - 30982.a6d605c291ff090be83b.js
    - 34426.589b6139887b40d85a8d.js
    - 41831.ad048c0163425aea4d2e.js
    - 43870.0bfb9a80f88725fba82a.js
    - 21251.b9123e8e72b94b26f3d9.js
    - 48590.3c8ba7ec46e5c315c058.js
    - 86480.ebf8826a7f33e22a6aba.js
    - 18407.d0257553d76c1da19de7.js
    - 37580.f4011cf1c76f3c28f15f.js
    - 75676.8481ee3ef6c0d7c670c6.js
    - 99742.0e4adf3d9577f74ac595.js
    - 47146.4dbf723f3a92ecba2ab7.js
    - 8240.6db5943ea811723a8032.js
    - 74970.29386fa1a51d34935ac6.js
    - 78193.59c58329071f325c3275.js
    - 74836.b991877dde75f9619c99.js
    - 32782.5d00908c88b18eb9ed1e.js
    - 94816.735d85d3cd81236d29b3.js
    - 37102.04489c88475d6b93636f.js
    - 60499.f4345dabc7337938ebde.js
    - 54314.0f71efde7d99ff587fee.js
    - 49191.5b151c54082714d18094.js
    - 22198.18946cfba71a0683afcd.js
    - 5399.67b6fe86854bee062580.js
    - 23777.9e9e7da77fb7df223e53.js
    - 77015.838feef2439d2d8cb461.js
    - 56145.8ff842e236afdf20ec13.js
    - 4453.dca020b41417f9b01ee6.js
    - 62768.6bd5173b2497c40223e0.js
    - 80083.7fd81fb4889aa662cd19.js
    - 66888.79756ea63981ab2a6341.js
    - 38081.70b6f68983fc3d77fdd3.js
    - 5687.a724fe30cb6ff469c19a.js
    - 89261.02ed5e657cba70087452.js
    - 26737.36ed5a81390b304d18a5.js
    - 81161.4cbf8610aaecd98b5550.js
    - 87140.141917e3b4392f49a9f0.js
    - 47498.38da6b2cf2f487359536.js
    - 29278.861b56f4090be4a1a9e4.js
    - 6086.2af42e57fcf6739db519.js
    - 64612.26d2bf1afbde26a43a76.js
    - 87475.49bdbe09116d150a15bd.js
    - 28413.4812d79703c86a531f70.js
    - 31421.ced40b898074b2c19b15.js
    - 77666.dee19f95c524dd006f98.js
    - 18409.089f6cdb9703ff04285b.js
    - 78891.49d43d53f7ab722777d3.js
    - 8608.36f3070721d34b25d956.js
    - 98346.a056de3caaaa893daf4a.js
    - 10716.bcccef800d16c68ae0c0.js
    - 12896.628bb944b26597841a9d.js
    - 41875.02628618e0c7e88c4368.js
    - 53509.d075f1bff85f12b95485.js
    - 68236.13baa29c5d54b14331be.js
    - 90856.923d21fae4e3dfbfbc4e.js
    - 7273.3c2a56f001bda14f0f32.js
    - 82451.afbcbd0aba0a3cf75eca.js
    - 65800.d803fbd4c225782b31d6.js
    - 90486.5c224c60cf8ae4287dfa.js
    - web.1a39a22bacb7803b1db2.js
    - 58409.1811376ebb7f14b0be53.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 25653.f1981721227784f0166e.js
    - 7273.3c2a56f001bda14f0f32.js
    - sentry.2c76c82b69502a4475d5.js
    
    Stylesheet:
    - shared.b26c3e34f71899dcfce5.css
    - app.936ae72980954bbdf974.css

--- current.js
+++ current.js
@@ -18201,7 +18201,7 @@
         L = E("782340");
       (0, i.setUpdateRules)(s.default), (0, n.default)(L.default, o.default, T.default), a.default.Emitter.injectBatchEmitChanges(r.batchUpdates), a.default.PersistedStore.disableWrites = __OVERLAY__, a.default.initialize();
       let u = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("263550", ", Version Hash: ").concat("3205d5d199b8ef0e5e663a0053837c6e73dd9503")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(u, ", Build Number: ").concat("263551", ", Version Hash: ").concat("9ce356d4b2363764613195d2f3965fe9ec9c5b04")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20444,8 +20444,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "263550", "263550"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("263550")), _ = 0), _
+        let _ = parseInt((e = "263551", "263551"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("263551")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -35806,4 +35806,4 @@
     }
   }
 ]);
-//# sourceMappingURL=90486.f75d38dc7529f7a3025c.js.map
+//# sourceMappingURL=90486.5c224c60cf8ae4287dfa.js.map
