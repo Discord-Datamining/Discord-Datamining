464ec8a288a7d76184c985755d81ed19cf7cc421
Tiemen <ThaTiemsz@users.noreply.github.com>
2024-02-13 19:32:31 +0100

    13 February 2024 - Build 265913 (c4a7035c8a1c652ed17071de2e3130456313bd16)
    Scripts:
    - shared.be49b422e25e3d3c4670.js
    - app.85334d1f2b1ca1cf8b8e.js
    - 22918.9f2b9d54bbfc371a4d92.js
    - 21396.259a270b7e3f8803a333.js
    - 95569.3cf9d01d9b7b6b85fb2b.js
    - 10991.d742d0d238c0d99e96ae.js
    - 34207.1a06e15c0a92805b2503.js
    - 71193.f90f4c123bff2fb2dec7.js
    - 482.6e1c86f88a37a71d42ec.js
    - 63550.a619020e4c7b3d5be7ac.js
    - 9065.a0b1cab93d4647fbb1a7.js
    - 73070.545b97290bac712ab8a7.js
    - 20117.7c4ea5cd4685b0442b9f.js
    - 58409.1811376ebb7f14b0be53.js
    - 31897.ec700144df6b20f401cb.js
    - 55695.1f3222e2a170c25c8fe1.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 64999.3c0486790babc24c66a4.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 53706.c13e855ace8621f4cbe9.js
    - 25653.f1981721227784f0166e.js
    - 25979.ba7e12ad3432f0dc414b.js
    - 46318.26a20b3d6c9d947ee7c5.js
    - 14875.19d6f00c81ee3b89074a.js
    - 75851.82c9a7f8176d778029e3.js
    - 5486.1bbb0b409999b3345c7d.js
    - 31717.eaaf09c49eb1cddcd35f.js
    - 34426.fe3567f62355ad9533d1.js
    - 80998.c24c270476896d1f3801.js
    - 47387.1e2640226df51a61466d.js
    - 48590.b50f2b3ffe22a6d6d948.js
    - 93492.93e6eb15d11f922eb940.js
    - 41831.5a17fc27202137362888.js
    - 43870.3017a682fcda0afc6fcc.js
    - 86480.7807f1597064c9cf1872.js
    - 18407.5d06991480d157949a91.js
    - 4468.ed3046d6ecbba9443419.js
    - 47146.14b04fbabb2e15d96fb8.js
    - 75676.d8003b2b37481e0aec12.js
    - 37580.270f065816fb045bc8cd.js
    - 77015.e501fce1199da0210645.js
    - 8240.47b1a0a8150481caead3.js
    - 98281.9fbbd252957b8bf44d74.js
    - 74836.ff680f90512a3312acaa.js
    - 84408.4ada388609f7ddfe0b3e.js
    - 44504.a24d96478100284b35e8.js
    - 58533.143d45d1fe30b5cb277f.js
    - 31337.faec46b0c4c63fe56821.js
    - 95185.5e98663c6a751ef7771e.js
    - 67079.7ebed3ea7dbe98696ad9.js
    - 49191.5b151c54082714d18094.js
    - 46493.947f25e22cfee913e1c9.js
    - 96412.88bf6bacca2e2c131f74.js
    - 23777.5dc40c4c2b6c28d07d6a.js
    - 94816.aa1ae4ac12b8f9da7dc0.js
    - 60499.9456f54ceef127044048.js
    - 37102.04489c88475d6b93636f.js
    - 23303.bb9a5614fc670630af1c.js
    - 69875.3da725de11b46c9b45d6.js
    - 15750.7a2933406d0e3b8b1aa6.js
    - 27505.c390eef7ed890465c8ff.js
    - 67690.949a40c153655ea7be23.js
    - 18814.335ed08939c971d35e24.js
    - 18788.a7561c504f326881e816.js
    - 50200.a081640643c8d63333df.js
    - 71554.032036a080cc27ba7fb5.js
    - 14388.1930c8323f52a5eba9d8.js
    - 32709.ebd1edbb790de9aa8265.js
    - 38081.2e687ba4efee360fcdbe.js
    - 96897.008f2a416a4c547f02a7.js
    - 26737.36ed5a81390b304d18a5.js
    - 81161.4cbf8610aaecd98b5550.js
    - 27043.201c4dfbc253f7adcadb.js
    - 47498.38da6b2cf2f487359536.js
    - 41039.cdc9782bcea69d47384e.js
    - 6086.a4df9b5e4bbf985d13d5.js
    - 64612.3464b23f78ef8294a4c6.js
    - 48059.2849a9b5b9eb8c414fa2.js
    - 55639.b623030b3c88f4915966.js
    - 94933.3bf4ec1f3331bd737b88.js
    - 18409.e219e4cffb96d18baffc.js
    - 78891.49d43d53f7ab722777d3.js
    - 88567.6e84dfaddfb3291ef55d.js
    - 807.a0d637ffc350c33b2de1.js
    - 55348.9f4d9bcb2bba94a62b76.js
    - 12896.80e0d6470bce67fa52c7.js
    - 41875.675e5c9cc5489ff409b3.js
    - 53509.d075f1bff85f12b95485.js
    - 23360.7f895565ed045cd5e80e.js
    - 15046.ea0127ebd61eb8e055fc.js
    - 7273.348bb05b7ba79a8318c3.js
    - 82451.afbcbd0aba0a3cf75eca.js
    - 65800.3061a650582d0001c16a.js
    - 73222.49f2d7652d70840aaad3.js
    - web.027a714126a0df48712e.js
    - 58409.1811376ebb7f14b0be53.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 25653.f1981721227784f0166e.js
    - 7273.348bb05b7ba79a8318c3.js
    - sentry.7cb9c489923fedb44ebc.js
    
    Stylesheet:
    - shared.3cc1b215623a0d378727.css
    - app.24f490c6249eca614f52.css

--- current.js
+++ current.js
@@ -18184,7 +18184,7 @@
         u = E("782340");
       (0, a.setUpdateRules)(s.default), (0, n.default)(u.default, o.default, T.default), i.default.Emitter.injectBatchEmitChanges(r.batchUpdates), i.default.PersistedStore.disableWrites = __OVERLAY__, i.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("265912", ", Version Hash: ").concat("6d1e3566809c83e3af4ef9f50ce0cca94c10cfbf")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("265913", ", Version Hash: ").concat("c4a7035c8a1c652ed17071de2e3130456313bd16")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -19024,6 +19024,70 @@
           })
         }) : null
       }
+    },
+    479732: function(e, _, E) {
+      "use strict";
+      E.r(_), E.d(_, {
+        default: function() {
+          return n
+        }
+      }), E("222007");
+      var t = E("91131"),
+        o = E("802493"),
+        n = new class e {
+          async getAll() {
+            let e = o.default.guildsRequiringDeletedIdsSync();
+            if (null == e) return new Set;
+            let _ = await e.getMany();
+            return new Set(_.map(e => e.id))
+          }
+          handleConnectionOpen(e, _) {
+            var E;
+            let {
+              guilds: n
+            } = e, r = n.filter(e => e.unableToSyncDeletes).map(e => ({
+              id: e.id
+            }));
+            r.length > 0 && o.default.guildsRequiringDeletedIdsSyncTransaction(_).putAll(r), null === (E = t.initialState.guildIdsRequiringDeletedIdsSync) || void 0 === E || E.forEach(e => {
+              o.default.guildsRequiringDeletedIdsSyncTransaction(_).put({
+                id: e
+              })
+            })
+          }
+          handleBackgroundSync(e, _) {
+            let {
+              guilds: E
+            } = e, t = E.filter(e => "partial" === e.data_mode && e.unableToSyncDeletes).map(e => ({
+              id: e.id
+            }));
+            t.length > 0 && o.default.guildsRequiringDeletedIdsSyncTransaction(_).putAll(t)
+          }
+          handleGuildCreate(e, _) {
+            let {
+              guild: E
+            } = e;
+            E.unableToSyncDeletes && o.default.guildsRequiringDeletedIdsSyncTransaction(_).put({
+              id: E.id
+            })
+          }
+          handleDeletedEntityIds(e, _) {
+            o.default.guildsRequiringDeletedIdsSyncTransaction(_).delete(e.guild_id)
+          }
+          handleClear(e) {
+            o.default.guildsRequiringDeletedIdsSyncTransaction(e).delete()
+          }
+          handleReset() {}
+          constructor() {
+            this.actions = {
+              CLEAR_CACHES: (e, _) => this.handleClear(_),
+              CLEAR_GUILD_CACHE: (e, _) => this.handleClear(_),
+              BACKGROUND_SYNC: (e, _) => this.handleBackgroundSync(e, _),
+              CONNECTION_OPEN: (e, _) => this.handleConnectionOpen(e, _),
+              GUILD_CREATE: (e, _) => this.handleGuildCreate(e, _),
+              DELETED_ENTITY_IDS: (e, _) => this.handleDeletedEntityIds(e, _)
+            }
+          }
+        }
     },
     805199: function(e, _, E) {
       "use strict";
@@ -20463,8 +20527,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "265912", "265912"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("265912")), _ = 0), _
+        let _ = parseInt((e = "265913", "265913"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("265913")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -23304,7 +23368,7 @@
         n = E("446674"),
         r = E("913144"),
         i = E("689988"),
-        a = E("288206"),
+        a = E("479732"),
         I = E("605250"),
         s = E("385976"),
         T = E("364685"),
@@ -23377,7 +23441,9 @@
       }
 
       function u() {
-        a.default.getGuildIdsRequiringDeletedIdsSync().forEach(e => C(e))
+        a.default.getAll().then(e => {
+          e.forEach(e => C(e))
+        })
       }
 
       function L(e) {
@@ -36341,4 +36407,4 @@
     }
   }
 ]);
-//# sourceMappingURL=73222.8dd8c8c16bb83859e923.js.map
+//# sourceMappingURL=73222.49f2d7652d70840aaad3.js.map
