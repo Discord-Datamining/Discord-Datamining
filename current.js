fb485976e68b18664c1f840f3405aecd31e36525
Tiemen <ThaTiemsz@users.noreply.github.com>
2024-02-24 03:11:25 +0100

    24 February 2024 - Build 268786 (05bbe5fddbd3ab0ca2c5e9135f12cca755732766)
    Scripts:
    - shared.6e41fec1e81eb0adba88.js
    - app.bf8d1403e86e2bd23343.js
    - 21396.259a270b7e3f8803a333.js
    - 95569.986be2e499cd6f7ea4e7.js
    - 80013.fccdb738bf0cecc34db7.js
    - 37144.05929cc88bb7d469799d.js
    - 71193.1e07abd0f3df83fa1828.js
    - 42966.91925ea3c48329678954.js
    - 46224.001f28ac67d0eec68d7b.js
    - 22918.9f2b9d54bbfc371a4d92.js
    - 9065.a0b1cab93d4647fbb1a7.js
    - 73070.545b97290bac712ab8a7.js
    - 20117.7c4ea5cd4685b0442b9f.js
    - 58409.1811376ebb7f14b0be53.js
    - 31897.ec700144df6b20f401cb.js
    - 14582.6109e8d6f8772c810375.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 64999.ef0a4ba5422c79916d68.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 53706.c13e855ace8621f4cbe9.js
    - 88957.f6c19ea946a6522831f8.js
    - 60247.025fdad3f5216e32bf68.js
    - 46318.26a20b3d6c9d947ee7c5.js
    - 14875.39b9f8cdf558e2bdb74c.js
    - 21251.a0e6aa60cfc1d59f1234.js
    - 31717.b2ff6588568eabfebf09.js
    - 34426.13b837c1d54aa323779b.js
    - 48590.795d840aee00d35739ee.js
    - 41831.a3fac9e35abf8bb49eca.js
    - 89904.ed6533746b3eaae34452.js
    - 72683.38c1641f7f338dade618.js
    - 28727.b8ac58c0c5d3ac709a52.js
    - 93492.aa355f688d52a2b9a9bf.js
    - 77015.dccb79a7b05af2dbf7f1.js
    - 43870.4a63d430baa0c834c3f9.js
    - 18407.3e22fc53a1eaa3bffb14.js
    - 37580.d26099f72612119e3df7.js
    - 74836.d4ce9301052f599814a1.js
    - 65256.398762930fea7fb6b340.js
    - 8240.ab58a6433a14007d827f.js
    - 61950.5ddcaf03cc4b743b7a77.js
    - 23777.054c6dc04bbfb90d4a6e.js
    - 98281.f2ff7c47bb45e261d995.js
    - 90904.8dddb08ee019ffe41161.js
    - 95185.868cc6e279c6ddb7836c.js
    - 67079.7ebed3ea7dbe98696ad9.js
    - 44504.285045bf277366f5ebd2.js
    - 31337.15b00870fc5304994f7d.js
    - 58533.18b20daadaa8142b6930.js
    - 37102.9ac9229dc4b8176c7dc8.js
    - 94726.b92e0fb0a16a886d5116.js
    - 60499.a83590003d6d6e70abe4.js
    - 60053.0bac6834ca0075b22c50.js
    - 98239.9cb590d0d5f18a0fd462.js
    - 86416.ff8b0c23f04993e97edd.js
    - 20258.22c525c8c490cd6ead84.js
    - 30791.88cd29e261bf94b04423.js
    - 60568.5d0c85f039bd8ffda60e.js
    - 18814.335ed08939c971d35e24.js
    - 12108.e238f3689ff5c75db314.js
    - 71554.5fad821a9d8ab8f0c9e3.js
    - 38081.86bb81bc07cc93da351c.js
    - 96897.008f2a416a4c547f02a7.js
    - 26737.36ed5a81390b304d18a5.js
    - 81161.74c7206ea3f33ef1435f.js
    - 27043.201c4dfbc253f7adcadb.js
    - 47498.38da6b2cf2f487359536.js
    - 21201.23e40a6e6e55916c2d6b.js
    - 6086.57eee01cfa97489d9289.js
    - 64612.9a1ddaa5961bd1fdc234.js
    - 48059.17d505fe2939b97bb9a7.js
    - 55639.b623030b3c88f4915966.js
    - 65656.e4daf5ab729dd2a4b5bf.js
    - 88567.688669b0dab0be57b538.js
    - 18409.c317d9e4bf33538ac00c.js
    - 78891.49d43d53f7ab722777d3.js
    - 4819.0dbeb5d1e4b29df9153f.js
    - 55348.86267527ca3d51194679.js
    - 12896.53aa859719fe56d857d7.js
    - 41875.80192922cde68798bd2a.js
    - 53509.d075f1bff85f12b95485.js
    - 23360.8d943b1704a9e3ccf72e.js
    - 79092.c687fcdb9f55324c8218.js
    - 7273.1d37c16f0157058afc1c.js
    - 1727.f828429b756c69401c80.js
    - 65800.e5783df1b5c507083376.js
    - 77761.a45040223451f4df4c1c.js
    - web.c2047897731ac5ef4b2d.js
    - 58409.1811376ebb7f14b0be53.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 88957.f6c19ea946a6522831f8.js
    - 7273.1d37c16f0157058afc1c.js
    - sentry.600f0aa68a99c932daa3.js
    
    Stylesheet:
    - shared.b1bf4ec54da1e08b2001.css
    - app.6d3b700561dbe1dffaa5.css

--- current.js
+++ current.js
@@ -15198,6 +15198,9 @@
         COLLECTIBLES_ELEMENTS_TOOLTIP_TITLE_ALT: "Element styles are here - which will you unleash?",
         COLLECTIBLES_ELEMENTS_COACHTIP_TITLE: "Harness the Elements",
         COLLECTIBLES_ELEMENTS_COACHTIP_DISCRIPTION: "Fire. Wind. Water. Earth. Which will you unleash?",
+        COLLECTIBLES_ANIME_V2_COACHTIP_TITLE: "New Anime items are here!",
+        COLLECTIBLES_ANIME_V2_COACHTIP_DISCRIPTION: "Check out the latest styles that will definitely make senpai notice you.",
+        COLLECTIBLES_NEW_BADGE: "NEW",
         INVENTORY_EMOJI_DETAILS_V2: "See emoji details",
         PRIVATE_CHANNEL_INTEGRATION_ADDED: "$[!!{username}!!](usernameHook) added the $[!!{otherUsername}!!](otherUsernameHook) app. See our [help center]({helpCenterLink}) for more info.",
         PRIVATE_CHANNEL_INTEGRATION_ADDED_DELETED_APPLICATION: "$[!!{username}!!](usernameHook) added a deleted application. See our [help center]({helpCenterLink}) for more info.",
@@ -18287,7 +18290,7 @@
         u = E("782340");
       (0, a.setUpdateRules)(s.default), (0, n.default)(u.default, o.default, T.default), i.default.Emitter.injectBatchEmitChanges(r.batchUpdates), i.default.PersistedStore.disableWrites = __OVERLAY__, i.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("268782", ", Version Hash: ").concat("5b7abe860a9ca58df3f914ed9d21769495d5c7a6")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("268786", ", Version Hash: ").concat("05bbe5fddbd3ab0ca2c5e9135f12cca755732766")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20625,8 +20628,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "268782", "268782"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("268782")), _ = 0), _
+        let _ = parseInt((e = "268786", "268786"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("268786")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -25337,8 +25340,8 @@
               body: {
                 metrics: e,
                 client_info: {
-                  built_at: "1708739923156",
-                  build_number: "268782"
+                  built_at: "1708740435922",
+                  build_number: "268786"
                 }
               },
               retries: 1
@@ -36595,4 +36598,4 @@
     }
   }
 ]);
-//# sourceMappingURL=77761.cfb55a3a75069a3fab0a.js.map
+//# sourceMappingURL=77761.a45040223451f4df4c1c.js.map
