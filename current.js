18bef06ae49cab7ce68367609c237281379a29fe
Tiemen <ThaTiemsz@users.noreply.github.com>
2024-03-01 23:15:40 +0100

    1 March 2024 - Build 271632 (3402eaa8360f08d2bcd3d72c03e9b496d4f199d4)
    Scripts:
    - shared.b7d70df8661fc770215d.js
    - app.61230a489da2fb810e5e.js
    - 21396.259a270b7e3f8803a333.js
    - 95569.986be2e499cd6f7ea4e7.js
    - 80013.fccdb738bf0cecc34db7.js
    - 37144.05929cc88bb7d469799d.js
    - 71193.70cfeb8a7cdca9bf1d62.js
    - 15318.29a18ca13f96a0e7f9ed.js
    - 46224.001f28ac67d0eec68d7b.js
    - 22918.9f2b9d54bbfc371a4d92.js
    - 9065.a0b1cab93d4647fbb1a7.js
    - 73070.545b97290bac712ab8a7.js
    - 20117.7c4ea5cd4685b0442b9f.js
    - 58409.1811376ebb7f14b0be53.js
    - 31897.ec700144df6b20f401cb.js
    - 14582.6109e8d6f8772c810375.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 64999.ef0a4ba5422c79916d68.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 53706.c13e855ace8621f4cbe9.js
    - 88957.f6c19ea946a6522831f8.js
    - 31081.441c968913e48b9257f8.js
    - 46318.26a20b3d6c9d947ee7c5.js
    - 28473.1bfa9a10548f9ad6fbf3.js
    - 14875.39b9f8cdf558e2bdb74c.js
    - 31717.23481df6ac9fb1a0d7ec.js
    - 74836.80f6b2b279aaee15e474.js
    - 47498.38da6b2cf2f487359536.js
    - 21251.a0e6aa60cfc1d59f1234.js
    - 34426.13b837c1d54aa323779b.js
    - 67731.42032df37145b7c728e1.js
    - 48590.f6eef6708acc002a23d9.js
    - 41831.a3fac9e35abf8bb49eca.js
    - 89904.9f9ef8324d9c17859563.js
    - 72683.82f5ca46f6e8e9d994cd.js
    - 28727.b8ac58c0c5d3ac709a52.js
    - 93492.aa355f688d52a2b9a9bf.js
    - 77015.0fd51d67dfaf077c89f3.js
    - 43870.4a63d430baa0c834c3f9.js
    - 18407.3e22fc53a1eaa3bffb14.js
    - 37580.c3446cb50b2e9b5e9e7f.js
    - 65256.6549e485c32382c9883e.js
    - 8240.67bfb1e7cae333296672.js
    - 61950.5ddcaf03cc4b743b7a77.js
    - 23777.151d691513040904bdd2.js
    - 98281.f2ff7c47bb45e261d995.js
    - 90904.8dddb08ee019ffe41161.js
    - 95185.833dba80ea987837d1c3.js
    - 67079.7ebed3ea7dbe98696ad9.js
    - 44504.285045bf277366f5ebd2.js
    - 69171.0c9c78de3e1dfecb1e5f.js
    - 58533.f4ff4fad22a5efef0d4e.js
    - 37102.9ac9229dc4b8176c7dc8.js
    - 94726.b92e0fb0a16a886d5116.js
    - 60499.a83590003d6d6e70abe4.js
    - 60053.48d5add9cd56943f80d3.js
    - 67615.eca979e2e25d4dec8ef7.js
    - 86416.ff8b0c23f04993e97edd.js
    - 20258.22c525c8c490cd6ead84.js
    - 30791.88cd29e261bf94b04423.js
    - 60568.5d0c85f039bd8ffda60e.js
    - 18814.335ed08939c971d35e24.js
    - 12108.e238f3689ff5c75db314.js
    - 71554.b98f9ed5339d3b6997ae.js
    - 38081.32681cd9dc8d81e5afbd.js
    - 96897.008f2a416a4c547f02a7.js
    - 26737.36ed5a81390b304d18a5.js
    - 81161.3f1fbfe502c7a08389e8.js
    - 27043.201c4dfbc253f7adcadb.js
    - 21201.f056654908895fc3645c.js
    - 6086.57eee01cfa97489d9289.js
    - 64612.9a1ddaa5961bd1fdc234.js
    - 48059.92ea26375a7a4cdbcf9b.js
    - 55639.f3cc9ab1f7c81c8b932c.js
    - 65656.854487ced5d2b760c8e5.js
    - 77434.08d2278e6605343f3df8.js
    - 18409.43b93e70c8f7b2ba053c.js
    - 78891.ce18a4638e53d286858d.js
    - 64228.e68a9bc875d26e9d8d1d.js
    - 77919.0b1f364675c42fb3c662.js
    - 55348.86267527ca3d51194679.js
    - 12896.e14674fd49155de3feba.js
    - 41875.80192922cde68798bd2a.js
    - 23360.8d943b1704a9e3ccf72e.js
    - 53509.d075f1bff85f12b95485.js
    - 92740.b7bea37e185a786abfc4.js
    - 7273.d1f2f00b578919400e52.js
    - 1727.bd25fcdd519ee470081c.js
    - 65800.5cfbe810c92672733f9d.js
    - 77761.8640bf07b8715a06e19f.js
    - web.a8c3837221b181ace335.js
    - 58409.1811376ebb7f14b0be53.js
    - 3341.1a1f8595a0c8fc9f99cf.js
    - 47470.91c2d89ebc10ef7a8bdd.js
    - 22843.1bda3edd4dd152273661.js
    - 94288.dbd73ecb6b1482a870b7.js
    - 88957.f6c19ea946a6522831f8.js
    - 7273.d1f2f00b578919400e52.js
    - sentry.e01bfae92ea2d6abced9.js
    
    Stylesheet:
    - shared.99503655a6ffa7a030a8.css
    - app.3a156331a8f58ea7b306.css

--- current.js
+++ current.js
@@ -17114,11 +17114,6 @@
         KEYBIND_NOTIFICATION_DESCRIPTION: "Now you can see your keybind for toggling mute in the overlay.",
         PROBLEMATIC_GPU_DRIVER_DETECTED: "There is currently a known issue with your GPU driver version which may affect the Go Live quality. If you experience this issue, learn more about how to fix it [here]({helpCenterLink}).",
         MANAGE_AUTHORIZED_APP: "Manage Authorized App",
-        ADD_TO_USER: "Add to my apps",
-        ADD_TO_USER_SUBTITLE: "Add app to your account to use anywhere",
-        ADD_TO_GUILD: "Add to server",
-        ADD_TO_GUILD_SUBTITLE: "Customize your server by adding this app",
-        USER_APPS_LEARN_MORE: "To learn more about apps on Discord, visit our [Help Center]({helpUrl}).",
         MFA_REQUIRED_FOR_STAFF: "Multi-factor authentication is required for staff.",
         INAPPROPRIATE_CONVERSATION_MARK_AS_TIER_1: "Add Inappropriate Tier 1 Warning (Experimental)",
         INAPPROPRIATE_CONVERSATION_MARK_AS_TIER_2: "Add Inappropriate Tier 2 Warning (Experimental)",
@@ -18328,7 +18323,7 @@
         u = E("782340");
       (0, a.setUpdateRules)(s.default), (0, n.default)(u.default, o.default, T.default), i.default.Emitter.injectBatchEmitChanges(r.batchUpdates), i.default.PersistedStore.disableWrites = __OVERLAY__, i.default.initialize();
       let L = window.GLOBAL_ENV.RELEASE_CHANNEL;
-      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("271631", ", Version Hash: ").concat("cfc3aba9e983e1ceb8000df170d40e8ddd03a55a")), t.default.setTags({
+      new(0, A.default)().log("[BUILD INFO] Release Channel: ".concat(L, ", Build Number: ").concat("271632", ", Version Hash: ").concat("3402eaa8360f08d2bcd3d72c03e9b496d4f199d4")), t.default.setTags({
         appContext: l.CURRENT_APP_CONTEXT
       }), S.default.initBasic(), N.default.init(), I.FocusRingManager.init(), O.init(), (0, R.cleanupTempFiles)()
     },
@@ -20666,8 +20661,8 @@
 
       function o() {
         var e;
-        let _ = parseInt((e = "271631", "271631"));
-        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("271631")), _ = 0), _
+        let _ = parseInt((e = "271632", "271632"));
+        return Number.isNaN(_) && (t.default.captureMessage("Trying to open a changelog for an invalid build number ".concat("271632")), _ = 0), _
       }
     },
     990629: function(e, _, E) {
@@ -25340,8 +25335,8 @@
               body: {
                 metrics: e,
                 client_info: {
-                  built_at: "1709330982686",
-                  build_number: "271631"
+                  built_at: "1709330986015",
+                  build_number: "271632"
                 }
               },
               retries: 1
@@ -36645,4 +36640,4 @@
     }
   }
 ]);
-//# sourceMappingURL=77761.d71352599051075c5117.js.map
+//# sourceMappingURL=77761.8640bf07b8715a06e19f.js.map
