ffab96b06323fdf6183b1f54345de5b762395f3a
Tiemen <ThaTiemsz@users.noreply.github.com>
2024-05-30 18:45:24 +0200

    30 May 2024 - Build 297402 (29c206e36f7be61686bf18b64c6a0a9cc17e623c)
    Scripts:
    - shared.f9da9e55d0f3116091d3.js
    - app.7e18079453e249dce619.js
    - 24217.fbecbc1d6a974fbc777e.js
    - 62734.1842567f24c7a0ab79a3.js
    - 43455.8c79ce3e1753b38de4a4.js
    - 10586.3f509a5d474354a36c24.js
    - 64787.359c4aba4bf61ba67cc0.js
    - 2797.a012718ee3dfd4179128.js
    - 30638.fdc3b8401571dddfd5fa.js
    - 17764.2aa7ee221234529f6e80.js
    - 84471.46bdd09b4cbd452e7ee2.js
    - 70397.226bb847204914e85d62.js
    - 27519.e78a74a1c1e7b3731699.js
    - web.84c9538099b4a9c5e408.js
    - 24217.fbecbc1d6a974fbc777e.js
    - 62734.1842567f24c7a0ab79a3.js
    - 43455.8c79ce3e1753b38de4a4.js
    - 84471.46bdd09b4cbd452e7ee2.js
    - sentry.765b00e66783ff42fca1.js
    
    Stylesheet:
    - 49237.46658cba45536161dba6.css
    - 99387.2051b922a13c24c24731.css

--- current.js
+++ current.js
@@ -2622,7 +2622,7 @@
           dsn: "https://fa97a90475514c03a42f80cd36d147c4@sentry.io/140984",
           autoSessionTracking: !1,
           environment: window.GLOBAL_ENV.RELEASE_CHANNEL,
-          release: "discord_web-bab17d0b6afb2e7b12369fc6a72e8f89afe14313",
+          release: "discord_web-29c206e36f7be61686bf18b64c6a0a9cc17e623c",
           beforeSend: _ => {
             var E, e;
             return !(null != (E = _).exception && null != E.exception.values && E.exception.values.every(_ => null == _.stacktrace || null != _.stacktrace.frames && 1 === _.stacktrace.frames.length) && "canary" !== window.GLOBAL_ENV.RELEASE_CHANNEL || a.some(_ => window.navigator.appVersion.toLowerCase().indexOf(_) >= 0)) && !R() && !("Aborted" === (e = _).message || "cancel captcha" === e.message) && t() ? _ : null
@@ -2640,7 +2640,7 @@
           })],
           ignoreErrors: ["EADDRINUSE", "BetterDiscord", "EnhancedDiscord", "Powercord", "RecipeWebview", "jQuery", "localStorage", "has already been declared", "Cannot call hover while not dragging.", "Cannot call beginDrag while dragging.", "getHostNode", "setupCSS", "on missing remote object", "ChunkLoadError", "Cannot find module 'discord_utils'", "Failed to setup Krisp module", "Error invoking remote method 'DISCORD_NATIVE_MODULES_INSTALL': Error: Module updater is not available!", "Non-Error promise rejection captured with keys:", "Request has been terminated", "Cannot resolve a Slate point from DOM point", "Failed to fetch", "no suitable image found", "ResizeObserver loop limit exceeded", "ResizeObserver loop completed with undelivered notifications.", "The play() request was interrupted", "could not play audio", "notosans-400-normalitalic"],
           denyUrls: [/recaptcha/, /mobilediscord\.com/, /betterdiscord:\/\//]
-        }), I.setTag("buildNumber", (_ = "297389", "297389")), I.setTag("builtAt", String("1717085867324"));
+        }), I.setTag("buildNumber", (_ = "297402", "297402")), I.setTag("builtAt", String("1717087130578"));
         let E = window.GLOBAL_ENV.SENTRY_TAGS;
         if (null != E && "object" == typeof E)
           for (let _ in E) I.setTag(_, E[_]);
@@ -14204,4 +14204,4 @@
     }
   }
 ]);
-//# sourceMappingURL=84471.8e4826486636786a0370.js.map
+//# sourceMappingURL=84471.46bdd09b4cbd452e7ee2.js.map
